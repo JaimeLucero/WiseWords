d543818b2830a6ef3089574b7cdaa63717aa77ac
JaimeLucero <jaimeemanuellucero@gmail.com>
2023-11-30 09:20:52 +0800

    tsv to avl init

--- lib/avltree/initalize_avl.dart
+++ lib/avltree/initalize_avl.dart
@@ -1,7 +1,46 @@
 import 'package:quiver/collection.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:wise_words/avltree/proverb.dart';
 
-class AvlData {
-  AvlTreeSet tree = AvlTreeSet();
+class AvlData extends AvlTreeSet<Proverb> {
+  bool done = false;
 
-  AvlData() {}
+  AvlData() {
+    readTSVFile('assets/data/data_set_test.tsv');
+  }
+
+  //read dataset file, async to execute in separate thread
+  Future<void> readTSVFile(String filePath) async {
+    String data = await rootBundle.loadString(filePath);
+
+    CsvToListConverter converter =
+        const CsvToListConverter(fieldDelimiter: '\t'); //using tab as delimiter
+    List<List<dynamic>> rowsAsList =
+        converter.convert(data); //splits data into 2d array of rows
+
+    //initialize each row as a proverb object
+    for (List<dynamic> row in rowsAsList) {
+      initProverb(int.parse(row[0].toString()), int.parse(row[1].toString()),
+          row[2], row[3], row[4]);
+    }
+
+    done = true; //sets true when the file is done reading
+  }
+
+  initProverb(int chapter, int verse, String unformatted, String formatted,
+      String keywords) {
+    List<String> keywordsList =
+        keywords.split(','); //splits the keywords into a list
+
+    Proverb proverb =
+        Proverb(chapter, verse, unformatted, formatted, keywordsList);
+
+    //adds the proverb into the AVL tree as a node
+    super.add(proverb);
+  }
+
+  bool isDone() {
+    return done;
+  }
 }
--- lib/avltree/proverb.dart
+++ lib/avltree/proverb.dart
@@ -1,4 +1,4 @@
-class Proverb {
+class Proverb implements Comparable<Proverb>{
   int chapter = 0;
   int verse = 0;
   String unformatted = '';
@@ -6,4 +6,19 @@ class Proverb {
   List<String> keywords = [];
 
   Proverb(this.chapter, this.verse, this.unformatted, this.formatted, this.keywords);
+
+  @override
+  int compareTo(Proverb other) { //compare verses for inserting
+    
+    if (chapter != other.chapter) { //compare by chapter first
+      return chapter.compareTo(other.chapter);
+    } else { //compare by verse next
+      return verse.compareTo(other.verse);
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Proverb{chapter: $chapter, verse: $verse, unformatted: $unformatted, formatted: $formatted, keywords: $keywords}';
+  }
 }
