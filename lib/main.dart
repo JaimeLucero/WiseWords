884795dc47dc4b481db499664b9e1237e17e438a
JaimeLucero <jaimeemanuellucero@gmail.com>
2023-11-30 09:20:52 +0800

    tsv to avl init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:wise_words/avltree/initalize_avl.dart';
 import 'package:wise_words/views/welcome_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AvlData();
   runApp(const MaterialApp(home: WelcomeView()));
 }
